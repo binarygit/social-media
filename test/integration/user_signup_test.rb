073a912ab7d135814d78a3658086f38314be1f4e
binarygit <inscription4@outlook.com>
2022-07-27 17:06:12 +0545

    Create signup

---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  test "signup with invalid information" do
+    get signup_path
+    assert_response :success
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { 
+                         user: {
+                           first_name: "",
+                           last_name: "",
+                           email: "",
+                           password: ""
+                         }
+                       }
+    end
+  end
+
+  test "signup with valid information" do
+    get signup_path
+    assert_response :success
+
+    assert_difference 'User.count' do
+      post users_path, params: { 
+                         user: {
+                           first_name: "Avi",
+                           last_name: "Bhurtel",
+                           email: "avi@avi.com",
+                           password: "foobar"
+                         }
+                       }
+    end
+  end
+end
