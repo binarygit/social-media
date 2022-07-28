566e930aaf2712a0022da48908aa4dca331341cc
binarygit <inscription4@outlook.com>
2022-07-28 17:03:07 +0545

    Stop rendering field with errors

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module SocialMedia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Stop rendering field with errors
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      html_tag
+    }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
