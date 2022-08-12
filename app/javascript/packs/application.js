3db2973104b78cba940a908f68f4cb66bcb19535
binarygit <inscription4@outlook.com>
2022-08-12 15:32:06 +0545

    Install mrujs and turbo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import mrujs from "mrujs";
+import * as Turbo from "@hotwired/turbo";
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbolinks.start()
+window.Turbo = Turbo;
+mrujs.start()
 ActiveStorage.start()
 
 import "controllers"
