2bd4ecc96faf2a90c565c1e6897d6affeef7f00b
binarygit <inscription4@outlook.com>
2022-08-21 13:58:05 +0545

    Verify req is original before creating

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,9 +1,18 @@
 class FriendRequestsController < ApplicationController
+  before_action :verify_original_req, only: [:create]
+
   def create
-    receiver = User.find(params[:receiver_id])
-    unless current_user.sent_friend_reqs.to(receiver) && receiver.sent_friend_reqs.to(current_user)
-      current_user.sent_friend_reqs.create!(receiver_id: params[:receiver_id])
-    end
+    current_user.sent_friend_reqs.create!(receiver_id: params[:receiver_id])
     redirect_to user_path(params[:receiver_id])
+  end
+
+  private
+
+  def verify_original_req 
+    receiver = User.find(params[:receiver_id])
+
+    if current_user.sent_friend_reqs.to(receiver) || receiver.sent_friend_reqs.to(current_user)
+      redirect_to user_path(params[:receiver_id])
+    end
   end
 end
