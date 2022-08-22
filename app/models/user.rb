031f9fe3daf75f8ec0733adcbaaa285ea2ecd186
binarygit <inscription4@outlook.com>
2022-08-22 22:14:14 +0545

    Create friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :received_friend_reqs, class_name: 'FriendRequest', foreign_key: 'receiver_id'
   has_many :friend_req_senders, through: :received_friend_reqs, source: :sender
 
+  has_many :friends, class_name: 'Friendship'
+
   validates :first_name, :last_name, presence: true
   validates :password, presence: true, allow_nil: true
   validates :email, presence: true, uniqueness: true
